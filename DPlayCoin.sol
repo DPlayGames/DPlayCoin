4aa8f1ed4fd845feed2d5aa7c10eefde46b9eece
codingcomposer <43369346+codingcomposer@users.noreply.github.com>
2019-03-12 15:09:15 +0900

    Update DPlayCoin.sol

--- DPlayCoin.sol
+++ DPlayCoin.sol
@@ -9,6 +9,7 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	using SafeMath for uint256;
 	
 	// 토큰 정보
+	// Token information 
 	string constant public NAME = "DPlay Coin";
 	string constant public SYMBOL = "DC";
 	uint8 constant public DECIMALS = 18;
@@ -29,6 +30,7 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	}
 	
 	// 주소를 잘못 사용하는 것인지 체크
+	// Checks if the address is misued. 
 	function checkAddressMisused(address target) internal view returns (bool) {
 		return
 			target == address(0) ||
@@ -36,34 +38,41 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	}
 	
 	//ERC20: 토큰의 이름 반환
+	//ERC20: Returns the name of the token. 
 	function name() external view returns (string memory) {
 		return NAME;
 	}
 	
 	//ERC20: 토큰의 심볼 반환
+	//ERC20: Returns the symbol of the token.
 	function symbol() external view returns (string memory) {
 		return SYMBOL;
 	}
 	
 	//ERC20: 토큰의 소수점 반환
+	//ERC20: Returns the decimals of the token.
 	function decimals() external view returns (uint8) {
 		return DECIMALS;
 	}
 	
 	//ERC20: 전체 토큰 수 반환
+	//ERC20: Returns the total number of tokens.
 	function totalSupply() external view returns (uint256) {
 		return TOTAL_SUPPLY;
 	}
 	
 	//ERC20: 특정 유저의 토큰 수를 반환합니다.
+	//ERC20: Returns the number of tokens of a specific user.
 	function balanceOf(address user) external view returns (uint256 balance) {
 		return balances[user];
 	}
 	
 	//ERC20: 특정 유저에게 토큰을 전송합니다.
+	//ERC20: Transmits tokens to a specific user.
 	function transfer(address to, uint256 amount) external payable returns (bool success) {
 		
 		// 주소 오용 차단
+		// Blocks misuse of an address.
 		require(checkAddressMisused(to) != true);
 		
 		require(amount <= balances[msg.sender]);
@@ -77,6 +86,7 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	}
 	
 	//ERC20: spender에 amount만큼의 토큰을 보낼 권리를 부여합니다.
+	//ERC20: Grants rights to send the amount of tokens to the spender.
 	function approve(address spender, uint256 amount) external payable returns (bool success) {
 		
 		allowed[msg.sender][spender] = amount;
@@ -87,11 +97,14 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	}
 	
 	//ERC20: spender에 인출을 허락한 토큰의 양을 반환합니다.
+	//ERC20: Returns the quantity of tokens to the spender
+	// spender가 인출하도록 허락 받은 토큰의 양을 반환합니다.
 	function allowance(address user, address spender) external view returns (uint256 remaining) {
 		return allowed[user][spender];
 	}
 	
 	//ERC20: 허락된 spender가 from으로부터 amount만큼의 토큰을 to에게 전송합니다.
+	//ERC20: The allowed spender sends the "amount" of tokens from the "from" to the "to".
 	function transferFrom(address from, address to, uint256 amount) external payable returns (bool success) {
 		
 		// 주소 오용 차단
@@ -111,6 +124,7 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	}
 	
 	//ERC165: 주어진 인터페이스가 구현되어 있는지 확인합니다.
+	//ERC165: Checks if the given interface has been implemented.
 	function supportsInterface(bytes4 interfaceID) external view returns (bool) {
 		return
 			// ERC165
@@ -121,11 +135,13 @@ contract DPlayCoin is ERC20, ERC165, NetworkChecker {
 	}
 	
 	// DC 파워를 반환합니다.
+	// Returns the DC power.
 	function getPower(address user) external view returns (uint256 power) {
 		return balances[user];
 	}
 	
 	// 테스트용 DC를 생성합니다.
+	// Creates DCs for testing.
 	function createDCForTest(uint256 amount) external {
 		if (network != Network.Mainnet) {
 			balances[msg.sender] += amount;
