a7bff93851652d7950b563c0d1ff57db6d015ae4
Hanul <hanul@hanul.me>
2019-06-05 17:21:28 +0900

    불필요한 스마트 계약 제거

--- Spy/KovanSpy.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.5.1;
-
-contract KovanSpy {
-	function helloKovan() pure external {} 
-}
--- Spy/RopstenSpy.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.5.1;
-
-contract RopstenSpy {
-	function helloRopsten() pure external {} 
-}
